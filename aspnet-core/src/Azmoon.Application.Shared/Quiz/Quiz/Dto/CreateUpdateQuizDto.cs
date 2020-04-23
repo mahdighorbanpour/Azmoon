4eddbdcdf991115aca44354749c9fa75597fed4d
Mahdi GhorbanPour <mahdi.ghorbanpour@ptw.de>
2020-04-23 07:55:33 +0200

    Implemented quiz admin service backend

---
+++ aspnet-core/src/Azmoon.Application.Shared/Quiz/Quiz/Dto/CreateUpdateQuizDto.cs
@@ -0,0 +1,22 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Azmoon.Application.Shared.Quiz.Quiz.Dto
+{
+    [AutoMapTo(typeof(Core.Quiz.Entities.Quiz))]
+    public class CreateUpdateQuizDto : EntityDto<Guid>
+    {
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Description { get; set; }
+
+        public TimeSpan? Duration { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ aspnet-core/src/Azmoon.Application.Shared/Quiz/Quiz/Dto/QuizDto.cs
@@ -0,0 +1,24 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Azmoon.Application.Shared.Quiz.Quiz.Dto
+{
+    [AutoMapFrom(typeof(Core.Quiz.Entities.Quiz))]
+    public class QuizDto : EntityDto<Guid>
+    {
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; }
+
+        public TimeSpan? Duration { get; set; }
+        public bool IsActive { get; set; }
+        public int TotalQuestion { get; set; }
+        public int TotalMarks { get; set; }
+    }
+}
