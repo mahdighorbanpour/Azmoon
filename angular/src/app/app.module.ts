76559dc05458fe0a5784cb30b493cd40732134ad
Mahdi GhorbanPour <mahdi.ghorbanpour@ptw.de>
2020-05-03 00:21:54 +0200

    fixed app sidebar

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -17,15 +17,11 @@ import { SharedModule } from '@shared/shared.module';
 
 import { HomeComponent } from '@app/home/home.component';
 import { AboutComponent } from '@app/about/about.component';
-import { SideBarUserAreaComponent } from '@app/layout/sidebar-user-area.component';
-import { SideBarNavComponent } from '@app/layout/sidebar-nav.component';
-import { SideBarFooterComponent } from '@app/layout/sidebar-footer.component';
 import { RightSideBarComponent } from '@app/layout/right-sidebar.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { NavBarUserAreaComponent } from './layout/navbar/navbar-user-area.component';
 import { NavBarLanguageSwitchComponent } from './layout/navbar/navbar-languageswitch.component';
 
@@ -34,9 +30,6 @@ import { NavBarLanguageSwitchComponent } from './layout/navbar/navbar-languagesw
     AppComponent,
     HomeComponent,
     AboutComponent,
-    SideBarUserAreaComponent,
-    SideBarNavComponent,
-    SideBarFooterComponent,
     RightSideBarComponent,
     FooterComponent,
     NavbarComponent,
@@ -52,7 +45,6 @@ import { NavBarLanguageSwitchComponent } from './layout/navbar/navbar-languagesw
     HttpClientJsonpModule,
     ModalModule.forRoot(),
     CollapseModule.forRoot(),
-    //BsDropdownModule.forRoot(),
     AbpModule,
     AppRoutingModule,
     ServiceProxyModule,
