04c220a95fc3cbd0f683e0de3fd64798e7aca782
Mahdi GhorbanPour <mahdi.ghorbanpour@ptwdosimetry.com>
2020-05-16 17:00:20 +0200

    added choiceDtos

---
+++ aspnet-core/src/Azmoon.Application.Shared/Quiz/Questions/Dto/ChoiceDto.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using Azmoon.Core.Quiz.Entities;
+using System;
+
+namespace Azmoon.Application.Shared.Quiz.Questions.Dto
+{
+    [AutoMapFrom(typeof(Choice))]
+    public class ChoiceDto: EntityDto<Guid>
+    {
+        public Guid QuestionId { get; set; }
+        public string Value { get; set; }
+        public bool IsCorrect { get; private set; } = false;
+    }
+
+
+}
